b4e6f5808a9bd9a537fa0f4cab4647e1858e96fb
bharathviswa2612 <bharathviswa2612@gmail.com>
2026-04-17 21:12:14 +0530

    remove the condition

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,22 +4,7 @@ package org.example;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-            System.out.println("Hello World");
-            findStringLength();
-            reverseStringLength();
-        }
-
-        public  static void findStringLength(){
-         String s = "Bharath";
-         int length = s.length();
-         System.out.println("The length of the string : "+length);
-        }
-
-    public  static void reverseStringLength(){
-        String s = "Bharath";
-        for(int i=0; i<s.length(); i++){
-            System.out.print(s.charAt(i));
-        }
+        System.out.println("Hello World");
     }
 
 }
